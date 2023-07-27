631f081d5a7e51aa752cea3f86010b5c53a8b0e7
Boris Sivko <boris.sivko@rapidfunnel.com>
2023-07-27 21:21:40 +0300

    user get in memory DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class UsersService {
+
+  private readonly users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
+    const now = Date.now();
+    this.users.push({
+      ...createUserDto,
+      id: crypto.randomUUID(),
+      version: 1,
+      createdAt: now,
+      updatedAt: now,
+    });
     return `This action adds a new user ${createUserDto.login}`;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.users.find((p) => p.id === id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
