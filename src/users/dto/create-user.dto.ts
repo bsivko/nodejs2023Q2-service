175873040f37f185d3ae171cedba06fa74779742
Boris Sivko <boris.sivko@rapidfunnel.com>
2023-07-27 20:35:25 +0300

    cleaned up

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,4 @@
 export class CreateUserDto {
-    readonly id: string;
-    readonly login: string;
-    readonly password: string;
-    readonly version: number; // integer number, increments on update
-    readonly createdAt: number; // timestamp of creation
-    readonly updatedAt: number; // timestamp of last update
+    login: string;
+    password: string;
 }
