3282109a0f5a5a543bde4ec7bc185d1d920e34de
Boris Sivko <boris.sivko@rapidfunnel.com>
2023-07-27 20:27:03 +0300

    +env as a file

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    readonly id: string;
+    readonly login: string;
+    readonly password: string;
+    readonly version: number; // integer number, increments on update
+    readonly createdAt: number; // timestamp of creation
+    readonly updatedAt: number; // timestamp of last update
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createUserDto: CreateUserDto): string {
     return this.usersService.create(createUserDto);
   }
 
